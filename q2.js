415233a950b9fbf617f9763d41bcfe50e686e0a4
TALHA HAROON <152915208+talha765haroon@users.noreply.github.com>
2024-02-25 03:07:57 +0500

    Add files via upload

---
+++ q2.js
@@ -0,0 +1,8 @@
+"use strict";
+// question no 2...assigment
+Object.defineProperty(exports, "__esModule", { value: true });
+/*ques # 02:
+personal message:  store a person' name  in a variable , and a print a message to that person, your message should be simple, such as, "Hello Eric,
+would you like to learn some  python today?"*/
+let firstName = "Talha Haroon";
+console.log(`Hello ${firstName}, "would you like to learn some Typscript today"?`);
---
+++ q2.ts
@@ -0,0 +1,9 @@
+// question no 2...assigment
+
+/*ques # 02:
+personal message:  store a person' name  in a variable , and a print a message to that person, your message should be simple, such as, "Hello Eric,
+would you like to learn some  python today?"*/
+
+let firstName:string = "Talha Haroon";
+
+console.log(`Hello ${firstName}, "would you like to learn some Typscript today"?`);
